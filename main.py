1cadecd5ebf090e025716ae933353ebf45b97c8c
CFranx <cfranks4@hawkmail.hccfl.edu>
2024-06-16 22:44:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+print("Outputs for A:\n")
+#Answer A
+a = 1
+b = 2
+c = 5
+while a < c:
+  a = a + 1
+  b = b + c
+
+print(a, b, c)
+
+print(" \nOutputs for B:\n ") 
+#Answer B
+d = 4
+e = 6
+f = 7
+while d > f:
+  d = d + f
+  e = e - 1
+print(d, e, f)
+
+print("\nOutputs for C:\n")
+#Answer C
+g = 4
+h = 6
+while g < h:
+  g = g + 1
+print(g, h)
+
+print("\nOutputs for D:\n")
+#Answer D
+j = 2
+k = 5
+n = 9
+while j < k:
+  m = 6
+  while m < n:
+    print("Goodbye")
+    m = m + 1
+  j = j + 1
+
+print("\nOutputs for E:\n")
+#Answer E
+j = 2
+k = 5
+m = 6
+n = 9
+while j < k:
+  while m < n:
+    print("Hello")
+    m = m + 1
+  j = j + 1
+
+print("\nOutputs for F:\n")
+#Answer F
+p = 2
+q = 4
+while p < q:
+  print("Adios")
+  r = 1
+  while r < q:
+    print("Adios")
+    r = r + 1
+  p = p + 1
